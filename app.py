f154fabea3f2527c83be73e19a0b0f869c487767
Haris <paris.krzysztof@gmail.com>
2026-08-10 17:45:45 +0000

    Initial Flask application

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return "DevSecOps lab"
+
+
+@app.route("/hello")
+def hello():
+    name = request.args.get("name", "world")
+    return f"Hello, {name}!"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
